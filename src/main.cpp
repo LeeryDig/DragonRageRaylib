36aa4f0568c5051ee8ef7cc7c3be246a8bc93731
Guilherme Cruz <guigacgui99@gmail.com>
2025-02-21 14:23:41 -0300

    Simple scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+#include <string>
+
 #include <raylib.h>
 #include <vector>
 #include "sysSettings.cpp"
+#include <math.h> 
 
 SysState sysState = SysState::PLAYING;
 
@@ -10,13 +13,28 @@ struct Road
     Vector3 size;
 };
 
+std::string Vector3ToString(const Vector3& vec) {
+    return "(" + std::to_string(vec.x) + ", " +
+                 std::to_string(vec.y) + ", " +
+                 std::to_string(vec.z) + ")";
+}
 
-int main()
-{
+float Lerp(float start, float end, float speed) {
+    float result = start + speed*(end - start);
+    return result;
+}
+
+float ConvertAngleToRadial(float angle) {
+    float result = angle * 3.14f/180;
+    return result;
+}
+
+
+int main() {
     InitWindow(1280, 720, "SPEED");
 
-    Camera camera = {{0.0f, 2.0f, 10.0f}, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, 1.0f, 0.0f}, 50.0f, 0};
-    Model model = LoadModel("resources/models/mach5.glb"); 
+    Camera camera = {{-0.12f, 0.4f, 10.11f}, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, 1.0f, 0.0f}, 50.0f, 0};
+    Model model = LoadModel("resources/models/Mach5.glb"); 
     float velocity = 0.5f;
 
     std::vector<Road> roads;
@@ -32,10 +50,11 @@ int main()
     roads.push_back({ {0, 0, -80}, {20, 0.1, 10}});
     roads.push_back({ {0, 0, -90}, {20, 0.1, 10}});
     roads.push_back({ {0, 0, -100}, {20, 0.1, 10}});
-    roads.push_back({ {0, 0, -110}, {20, 0.1, 10}});
 
-    Vector3 playerPosition = { 0.0f, 1.0f, 2.0f };
-    Vector3 playerSize = { 0.6f, 0.8f, 1.5f };
+    Vector3 playerPosition = { 0.0f, 0.2f, 10.0f };
+    Vector3 playerSize = { 2.0f, 2.0f, 2.0f };
+    Vector3 pRotationAxis = { 0.0f, 1.0f, 0.0f };
+    float pRotation = 0.0f;
     Color playerColor = GREEN;
 
     SetTargetFPS(60);
@@ -43,9 +62,42 @@ int main()
     {
         // Update
         // UpdateCamera(&camera, CAMERA_FREE);
+        // std::string cStr = Vector3ToString(camera.position);
+        // std::string pStr = Vector3ToString(playerPosition);
+        DisableCursor();
+        float radialPRotation = ConvertAngleToRadial(pRotation);
+
+        if (IsKeyDown(KEY_UP) && velocity < 0.85f) {
+            velocity += 0.008f;
+        }
+        else if (IsKeyDown(KEY_DOWN) && velocity > 0.2f) {
+            velocity -= 0.006f;
+        }
+
         // Move player
-        if (IsKeyDown(KEY_RIGHT)) playerPosition.x += 0.2f;
-        else if (IsKeyDown(KEY_LEFT)) playerPosition.x -= 0.2f;
+        if (IsKeyDown(KEY_RIGHT) && radialPRotation > -0.3f) {
+            pRotation -= 0.5;
+            float amout = cosf(radialPRotation) * 0.3f;
+            playerPosition.x += amout;
+            camera.position.x += amout;
+            camera.target.x += amout;
+        }
+        else if (IsKeyDown(KEY_LEFT) && radialPRotation < 0.3f) {
+            pRotation += 0.5;
+            float amout = cosf(radialPRotation) * 0.3f;
+            playerPosition.x -= amout;
+            camera.position.x -= amout;
+            camera.target.x -= amout;
+        }
+        else {
+            pRotation = Lerp(pRotation, 0.0f, 0.05f);
+        }
+        // else if (IsKeyDown(KEY_UP)) camera.position.y += 0.01f;
+        // else if (IsKeyDown(KEY_DOWN)) camera.position.y -= 0.01f;
+        // else if (IsKeyDown(KEY_A)) camera.position.x -= 0.01f;
+        // else if (IsKeyDown(KEY_D)) camera.position.x += 0.01f;
+        // else if (IsKeyDown(KEY_S)) camera.position.z -= 0.01f;
+        // else if (IsKeyDown(KEY_W)) camera.position.z += 0.01f;
         
         // Draw
         BeginDrawing();
@@ -53,13 +105,9 @@ int main()
             ClearBackground(RAYWHITE);
 
             BeginMode3D(camera);
+                DrawModelEx(model, playerPosition, pRotationAxis, pRotation, (Vector3){ 1.0f, 1.0f, 1.0f }, WHITE);
 
-                // DrawCubeV(playerPosition, playerSize, playerColor);
-                DrawModelEx(model, playerPosition, (Vector3){ 0.0f, 0.0f, 1.0f }, 0.0f, (Vector3){ 1.0f, 1.0f, 1.0f }, WHITE);
-
-
-                for (size_t i = 0; i < roads.size(); i++)
-                {
+                for (size_t i = 0; i < roads.size(); i++) {
                     roads[i].pos.z += velocity;
                     if (i % 2 == 0) {
                         DrawCube(roads[i].pos, roads[i].size.x, roads[i].size.y, roads[i].size.z, GRAY);
@@ -69,26 +117,24 @@ int main()
                     }
 
                     if (roads[i].pos.z >= 20) {
-                        roads[i].pos.z = -110.0f;
+                        roads[i].pos.z = -100.0f;
                     }
                 }
 
-                DrawGrid(10, 1.0f);        // Draw a grid
+                DrawGrid(100, 1.0f);
 
             EndMode3D();
 
-            DrawText("NICE GRAPHICS", 220, 40, 20, BLACK);
+            DrawText("NICE GRAPHICS", 10, 30, 10, BLACK);
+            DrawText(std::to_string(ConvertAngleToRadial(pRotation)).c_str(), 220, 80, 20, BLACK);
+            // DrawText(pStr.c_str(), 220, 100, 20, BLACK);
 
             DrawFPS(10, 10);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
 
     return 0;
 }
