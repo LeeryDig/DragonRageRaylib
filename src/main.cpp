ea1074e0292172e9fcbfb95c4c8722f3e9b1ffeb
Guilherme Cruz <guigacgui99@gmail.com>
2025-02-20 21:03:13 -0300

    Make a endless runner

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,94 @@
-namespace ray
-{
 #include <raylib.h>
-}
+#include <vector>
+#include "sysSettings.cpp"
 
-ray::Vector3 spherePos{0.0f, 0.0f, 0.0f};
+SysState sysState = SysState::PLAYING;
+
+struct Road
+{
+    Vector3 pos;
+    Vector3 size;
+};
+
 
 int main()
 {
-    ray::InitWindow(1250, 720, "NICE GRAPHICS");
+    InitWindow(1280, 720, "SPEED");
 
-    ray::Camera camera = {0};
-    camera.position = (ray::Vector3){0.0f, 10.0f, 10.0f};
-    camera.target = (ray::Vector3){0.0f, 0.0f, 0.0f};
-    camera.up = (ray::Vector3){0.0f, 1.0f, 0.0f};
-    camera.fovy = 45.0f;
-    camera.projection = ray::CAMERA_PERSPECTIVE;
+    Camera camera = {{0.0f, 2.0f, 10.0f}, (Vector3){0.0f, 0.0f, 0.0f}, (Vector3){0.0f, 1.0f, 0.0f}, 50.0f, 0};
+    Model model = LoadModel("resources/models/mach5.glb"); 
+    float velocity = 0.5f;
 
-    ray::Model model = ray::LoadModel("resources/models/Mazda.glb");
-    ray::Texture2D texture = ray::LoadTexture("resources/models/MazdaBodyDiff.png");
-    ray::SetMaterialTexture(&model.materials[0], ray::MATERIAL_MAP_DIFFUSE, texture);
-    ray::DisableCursor(); 
-    while (!ray::WindowShouldClose())
+    std::vector<Road> roads;
+    roads.push_back({ {0, 0, 10}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, 0}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -10}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -20}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -30}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -40}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -50}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -60}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -70}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -80}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -90}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -100}, {20, 0.1, 10}});
+    roads.push_back({ {0, 0, -110}, {20, 0.1, 10}});
+
+    Vector3 playerPosition = { 0.0f, 1.0f, 2.0f };
+    Vector3 playerSize = { 0.6f, 0.8f, 1.5f };
+    Color playerColor = GREEN;
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) 
     {
-        ray::UpdateCamera(&camera, ray::CAMERA_FIRST_PERSON);
+        // Update
+        // UpdateCamera(&camera, CAMERA_FREE);
+        // Move player
+        if (IsKeyDown(KEY_RIGHT)) playerPosition.x += 0.2f;
+        else if (IsKeyDown(KEY_LEFT)) playerPosition.x -= 0.2f;
+        
+        // Draw
+        BeginDrawing();
 
-        ray::BeginDrawing();
-        ray::ClearBackground(ray::RAYWHITE);
-            ray::BeginMode3D(camera);
-                ray::DrawText("NICE GRAPHICS", 625, 320, 30, ray::WHITE);
-                ray::DrawModelEx(model, spherePos, (ray::Vector3){ 1.0f, 0.0f, 0.0f }, 0.0f, (ray::Vector3){ 0.1f, 0.1f, 0.1f }, ray::WHITE);
-                ray::DrawGrid(10, 1.0f);
-            ray::EndMode3D();
-        ray::EndDrawing();
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                // DrawCubeV(playerPosition, playerSize, playerColor);
+                DrawModelEx(model, playerPosition, (Vector3){ 0.0f, 0.0f, 1.0f }, 0.0f, (Vector3){ 1.0f, 1.0f, 1.0f }, WHITE);
+
+
+                for (size_t i = 0; i < roads.size(); i++)
+                {
+                    roads[i].pos.z += velocity;
+                    if (i % 2 == 0) {
+                        DrawCube(roads[i].pos, roads[i].size.x, roads[i].size.y, roads[i].size.z, GRAY);
+                    }
+                    else {
+                        DrawCube(roads[i].pos, roads[i].size.x, roads[i].size.y, roads[i].size.z, LIGHTGRAY);
+                    }
+
+                    if (roads[i].pos.z >= 20) {
+                        roads[i].pos.z = -110.0f;
+                    }
+                }
+
+                DrawGrid(10, 1.0f);        // Draw a grid
+
+            EndMode3D();
+
+            DrawText("NICE GRAPHICS", 220, 40, 20, BLACK);
+
+            DrawFPS(10, 10);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    ray::UnloadTexture(texture);
-    ray::UnloadModel(model);
-
-    ray::CloseWindow();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
---
+++ src/sysSettings.cpp
@@ -0,0 +1,9 @@
+enum class SysState {
+    EDITOR,
+    MENU,
+    PLAYING,
+    PAUSED
+};
+
+const int SCRWIDTH = 1280;
+const int SCRHEIGHT = 720;
